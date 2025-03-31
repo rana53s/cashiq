9c787a23b23ad3ecd5073638e2f064b6d9842cce
Satyajit Paul <paulsjit@gmail.com>
2025-03-31 09:55:29 +0530

    Bun Fiexed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,58 +1,34 @@
-export const runtime = "edge";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <>
-      <title>404: This page could not be found.</title>
-      <div style={styles.error}>
-        <div>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-            }}
-          />
-          <h1 className="next-error-h1" style={styles.h1}>
-            404
-          </h1>
-          <div style={styles.desc}>
-            <h2 style={styles.h2}>This page could not be found.</h2>
-          </div>
-        </div>
+    <div className='h-screen bg-gradient-to-b from-red-900 via-red-700 to-red-600 flex flex-col items-center justify-center text-white text-center'>
+      <div className='mb-6'>
+        {/* You can reuse the same logo or use a different one for the 404 page */}
+        <Image
+          src='/android-chrome-512x512.png'
+          alt='Logo'
+          width={100}
+          height={100}
+        />
       </div>
-    </>
+
+      <h1 className='text-4xl font-extrabold'>404 - Page Not Found</h1>
+      <p className='mt-4 text-lg max-w-lg mx-auto'>
+        Oops! The page you&apos;re looking for doesn&apos;t exist or has been
+        moved.
+      </p>
+
+      <div className='mt-8 text-lg'>
+        <p>Let&apos;s get you back to safety!</p>
+        <Link
+          href='/'
+          className='mt-4 px-6 py-2 bg-indigo-500 text-white rounded-full hover:bg-indigo-700 transition-colors'
+        >
+          Go to Home
+        </Link>
+      </div>
+    </div>
   );
 }
-
-const styles = {
-  error: {
-    fontFamily:
-      'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-    height: "100vh",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  desc: {
-    display: "inline-block",
-  },
-
-  h1: {
-    display: "inline-block",
-    margin: "0 20px 0 0",
-    padding: "0 23px 0 0",
-    fontSize: 24,
-    fontWeight: 500,
-    verticalAlign: "top",
-    lineHeight: "49px",
-  },
-
-  h2: {
-    fontSize: 14,
-    fontWeight: 400,
-    lineHeight: "49px",
-    margin: 0,
-  },
-} as const;
